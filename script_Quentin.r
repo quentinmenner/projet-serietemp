8634fa2c0b9eb51d515fe509d1611623371ee0bb
AntoineJ35 <79692452+AntoineJ35@users.noreply.github.com>
2022-04-15 22:43:23 +0200

    Réponse aux questions de la Partie 1: différenciation et désaisonnalisé

--- script_Quentin.r
+++ script_Quentin.r
@@ -1,31 +1,64 @@
+###### Projet de série temporelle ######
+
+
+# Packages utilisés ####
+
 require(zoo)
 require(tseries)
+library(tidyverse)
 
-datafile <- "valeurs_mensuelles_2.csv"
-data <- read.csv(datafile,sep=";")
-data <- apply(data, 2, rev)
-data <- as.data.frame(data)
-data$Valeur <- as.numeric(data$Valeur)
-print ("Modified Dataframe")
-print (data)
+# Importation et structuration des données ####
+datafile <- "projet-serietemp/valeurs_mensuelles_2.csv"
+data <- read.csv(datafile, sep = ";") %>% arrange(Periode)
+ts_data = ts(data$Valeur, start = c(1985,01), freq = 12)
+plot(ts_data, type = "l")
 
-'x <- data[,c("Periode")]
-y <- data[,c("Valeur")]
-x <- c(x)
-plot(y)'
-#new <- as.Date(x, format="%Y-%m")
-#data$Periode <- as.Date(data$Periode, format="%Y-%m")
-data$Periode <- as.yearmon(data$Periode, "%Y-%m")
-#data$Periode <- as.Date(paste0(data$Periode, '01'), format="%Y-%m-%d")
+# Partie 1 ####
 
-plot(data$Periode,                                      # Draw plot without x-axis
-     data$Valeur,
-     type = "l",
-     xaxt = "n")
+acf(ts_data) # Il semble que la série représente un AR pure
+
+# Question 1
+
+# Testons la stationnarité avec un test ADF
+m <- fUnitRoots::adfTest(ts_data, type = "ct")
+m
+# On rejette fortement l'hypothèse de stationnarité
+# Une tendance à la hausse semble se dégager
+diff_ts = diff(ts_data, 1)
+
+plot(diff_ts)
+
+#Par ailleurs une saisonnalité semble s'observer (forte hausse à la fin de l'année)
+desaison_ts = diff(diff_ts,12)
+plot(desaison_ts)
+
+fUnitRoots::adfTest(desaison_ts, type = "c")
+# On rejette l'hypothèse de racine unitaire à 99% donc la série est stationnaire
+
+# Représentation de la série avant / après 
+library(patchwork)
+(plot.ts(ts_data))+
+  (plot.ts(diff_ts))
+
+# Partie 2 ####
+
+# Question 4 : Estimation un ARMA(p,q)
+acf(desaison_ts)
+# valeur max p est de 2
+
+pacf(desaison_ts)
+# valeur max q est de 3
+
+
+# Partie 3 ####
+
+
+
+
+
 #axis(1,                                                   # Add dates to x-axis
 #     data$Periode,
 #     format(data$Periode, "%Y-%m-%d"))
-axis(1, at = data$Periode[seq(1, length(data$Periode), 12)])
 #     labels = c("Some text", "Other text"))
 
-acf(data$Valeur)
+
