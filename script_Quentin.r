9154b07a12a1b67d6df416ddf2caffa0bdc3385d
AntoineJ35 <79692452+AntoineJ35@users.noreply.github.com>
2022-04-24 19:21:28 +0200

    Ajout graphique saison

--- script_Quentin.r
+++ script_Quentin.r
@@ -3,42 +3,70 @@
 
 # Packages utilisés ####
 
-require(zoo)
-require(tseries)
+library(zoo)
+library(tseries)
+library(lmtest)
+library(forecast)
+library(ggplot2)
+library(urca)
 library(tidyverse)
+library(lubridate)
+library(xts)
 
 # Importation et structuration des données ####
 datafile <- "projet-serietemp/valeurs_mensuelles_2.csv"
 data <- read.csv(datafile, sep = ";") %>% arrange(Periode)
 ts_data = ts(data$Valeur, start = c(1985,01), freq = 12)
 plot(ts_data, type = "l")
+# On observe une tendance à la hausse
+# Données : 1985 - 2000
 
 # Partie 1 ####
 
 acf(ts_data) # Il semble que la série représente un AR pure
 
-# Question 1
+## Question 1
+# Saisonnalité ? 
+decomposition_saison = data %>% 
+  mutate(year = str_sub(Periode, end = 4)) %>% 
+  mutate(month = str_sub(Periode, -2)) %>% 
+  group_by(month) %>% 
+  summarise(moy_mens = mean(Valeur))
 
-# Testons la stationnarité avec un test ADF
-m <- fUnitRoots::adfTest(ts_data, type = "ct")
-m
-# On rejette fortement l'hypothèse de stationnarité
+plot(decomposition_saison$moy_mens)
+#On observe une très légère saisonnalité
+(max(decomposition_saison$moy_mens) - min(decomposition_saison$moy_mens))/min(decomposition_saison$moy_mens)
+# Les valeurs de décembre sont en moyenne 1,5% supérieur à celle de février...
+
+# Tendance ?
+data_tendance = data %>% 
+  mutate(index = index(data))
+library("car")
+
+scatterplot(Valeur ~ index, data = data_tendance, 
+            smoother = FALSE, grid = FALSE, frame = FALSE)
+
 # Une tendance à la hausse semble se dégager
 diff_ts = diff(ts_data, 1)
 
 plot(diff_ts)
 
-#Par ailleurs une saisonnalité semble s'observer (forte hausse à la fin de l'année)
-desaison_ts = diff(diff_ts,12)
-plot(desaison_ts)
-
+# ADF test
+fUnitRoots::adfTest(diff_ts)
 fUnitRoots::adfTest(desaison_ts, type = "c")
-# On rejette l'hypothèse de racine unitaire à 99% donc la série est stationnaire
+
+# PP test
+tseries::pp.test(diff_ts) # test utilisé dans corrigé
+summary(urca::ur.pp(diff_ts, type="Z-tau"))
+summary(urca::ur.pp(diff_ts, type="Z-tau",model = "trend"))
+
+# KPSS
+tseries::kpss.test(diff_ts)
+
+# On rejette l'hypothèse de racine unitaire donc la série différencié est stationnaire
 
 # Représentation de la série avant / après 
 library(patchwork)
-(plot.ts(ts_data))+
-  (plot.ts(diff_ts))
 
 # Partie 2 ####
 
@@ -48,6 +76,14 @@ acf(desaison_ts)
 
 pacf(desaison_ts)
 # valeur max q est de 3
+
+# Testons cette hypothèses
+model_maxi <- arima(desaison_ts, order = c(2,0,3))
+residus_maxi <- residuals(model_maxi)
+# Parait bon :
+ggAcf(residus_maxi) + ggPacf(residus_maxi)
+
+portes::LjungBox(model_maxi, order = 6)
 
 
 # Partie 3 ####
