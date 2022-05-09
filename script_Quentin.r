252c9a3fff3502070462954721e577f4420950f1
AntoineJ35 <79692452+AntoineJ35@users.noreply.github.com>
2022-05-09 15:18:30 +0200

    Ajout de graphiques + fin rédaction partie 2

--- script_Quentin.r
+++ script_Quentin.r
@@ -13,6 +13,8 @@ library(tidyverse)
 library(lubridate)
 library(xts)
 library(patchwork)
+library(ggpmisc)
+
 
 # Lieu exportation graphiques
 setwd(dir = "~/projet-serietemp")
@@ -73,30 +75,41 @@ diff_ts = diff(ts_data, 1)
 
 # ADF test
 test_adf = urca::ur.df(diff_ts)
-summary(test_adf)
+slot(test_adf, 'teststat')
+slot(test_adf, "cval")
 # Résultat : "p-value: < 2.2e-16"
 # On rejette l'hypothése nulle (la série n'est pas stationnaire)
 
 # PP test
-test_pp = tseries::pp.test(diff_ts) # test utilisé dans corrigé
+test_pp = urca::ur.pp(diff_ts, type = "Z-tau") # test utilisé dans corrigé
+slot(test_pp, 'teststat')
+slot(test_pp, "cval")
 test_pp
+summary(test_pp)
 # Résultat : "p-value = 0.01"
 # On rejette l'hypothése nulle (la série n'est pas stationnaire)
 
 # KPSS
-test_kpss = urca::ur.kpss(diff_ts)
-summary(test_kpss)
+test_kpss = kpss.test(diff_ts, null = "Level")
+test_kpss
+slot(test_kpss, 'teststat')
+slot(test_kpss, "cval")
 # Résultat : "test-statistic is 0.207" ce qui est bien inférieur aux valeurs critiques usuelles
 # On ne rejette pas l'hypothése nulle (la série est stationnaire)
+
+# Rassemblement des données dans un tableau
+
+
 
 # La série différenciée est donc stationnaire.
 
 
-## Quesion 3 ####
+# Question 3 ####
 
 #Représentation de la série avant :
 
 #Graphique de la série en niveau
+
 graph_niveau = ggplot(data = data, aes(x = Periode, y = Valeur))+
   geom_line()+
   scale_x_date(expand = c(0.01, 0.01)) +
@@ -117,11 +130,9 @@ saveRDS(graph_niveau, file.path(lien_graph,"courbe.rds"))
 # Réprésentation de la série après :
 
 # Passage en object data.frame
-df_serie_choisi = data.frame(Periode = time(diff_ts),
-                             Valeur = as_tibble(diff_ts)$x)
 
 # Construction du graphique
-graph_diff = ggplot(df_serie_choisi, aes(x = Periode, y = Valeur))+
+graph_diff = ggplot(diff_ts, as.numeric = FALSE)+
   geom_line()+
   ggtitle("Représentation de la série temporelle choisie") +
   ggthemes::theme_stata()+
@@ -140,31 +151,48 @@ graph_niveau /
 
 # Partie 2 ####
 
-# Question 4 : Estimation un ARMA(p,q)
-ggAcf(diff_ts)
+# Question 4 : Choix ARMA(p,q) ####
+
+# Identification des valeurs maximales
+acf = ggAcf(diff_ts)+
+  ggthemes::theme_stata()+
+  labs(title = "ACF")+
+  theme(
+    plot.title   = element_text(lineheight = 0.8, face = "bold", hjust = 0.5, size = 15),
+    axis.text.x  = element_text(),
+    axis.text.y  = element_text(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank())
+
 # valeur max p est de 1 (pour le MA)
 
-ggPacf(diff_ts)
-# valeur max q est de 3 (pour le AR) en regardant assez largement
+pacf = ggPacf(diff_ts)+
+  ggthemes::theme_stata()+
+  labs(title = "PACF")+
+  theme(
+    plot.title   = element_text(lineheight = 0.8, face = "bold", hjust = 0.5, size = 15),
+    axis.text.x  = element_text(),
+    axis.text.y  = element_text(),
+    axis.title.x = element_blank(),
+    axis.title.y = element_blank())
 
-# Testons cette hypoth�ses
-model_maxi <- arima(diff_ts, order = c(1,0,3))
-residus_maxi <- residuals(model_maxi)
-# Parait bon :
-ggAcf(residus_maxi) + ggPacf(residus_maxi)
-# On observe un pique important pour un lag 12
+# Sauvegarde 
+saveRDS(acf, file.path(lien_graph,"ACF_ts_choix.rds"))
+saveRDS(pacf, file.path(lien_graph,"PACF_ts_choix.rds"))
 
-portes::LjungBox(model_maxi, order = 6)
-# Le mod�le maximal parait passer le test d'autocorr�lation des r�sidus.
+acf /
+  pacf
+# valeur maximale de q est 1 (pour le AR) 
 
-#Test des valeurs possibles de p et de q : test de tous les mod�les
-
-lmtest::coeftest(model_maxi) # Coefficient ar1 non significatif --> pas forc?ment utilisable
+# Estimation des modèles : test de tous les modèles
 
 evaluation_model <- function(order, x = diff_ts, lags = 24,...){
-  model <- forecast::Arima(x, order = order,...)
+  # On estime les paramètres
+  model = forecast::Arima(x, order = order,...)
+  
   residus <- residuals(model)
-  # Test d'autocorr�lation des r�sidus
+  
+  # Test d'autocorrélation des résidus avec lags retards testés
   lbtest <- t(sapply(1:lags,function(l){
     if(l <=  length(coef(model))){
       b <- list(statistic = NA, p.value = NA)
@@ -178,8 +206,10 @@ evaluation_model <- function(order, x = diff_ts, lags = 24,...){
                b$p.value
     )
   }))
-  # on ajoute un tryCatch pour �viter les erreurs
+  # on ajoute un tryCatch pour éviter les erreurs
   ttest <- tryCatch(lmtest::coeftest(model), error = NULL)
+  
+  # On regarde les informations de validité
   qualite <- c(AIC(model), BIC(model), accuracy(model))
   names(qualite) <- c("AIC", "BIC", colnames(accuracy(model)))
   list(model = model,
@@ -189,12 +219,12 @@ evaluation_model <- function(order, x = diff_ts, lags = 24,...){
   
 }
 
-models_possibles <- expand.grid(p = c(0, 1, 2), d = 0, q = c(0, 1, 2, 3))
 
-models_evalues <- apply(models_possibles,1, evaluation_model)
+models_possibles = expand.grid(p = c(0, 1), d = 0, q = c(0, 1))
 
-names(models_evalues) <- sprintf("ARIMA(%i,%i,%i)", models_possibles[,"p"],
-                                 models_possibles[,"d"], models_possibles[,"q"])
+# On applique la fonction au 4 modèles possibles
+models_evalues = apply(models_possibles,1, evaluation_model)
+
 
 models_evalues$`ARIMA(0,0,0)`
 # R�sidus non corr�l�s mais coefficient pas significatif
@@ -202,8 +232,6 @@ models_evalues$`ARIMA(0,0,0)`
 models_evalues$`ARIMA(1,0,0)`
 # R�sidus non corr�l�s globalement et coefficients significatif mod�le retenu
 
-models_evalues$`ARIMA(2,0,0)`
-# R�sidus non corr�l�s globalement mais coefficient ar2 pas significatif
 
 models_evalues$`ARIMA(0,0,1)`
 # R�sidus non corr�l�s globalement et coefficients significatifs
@@ -211,47 +239,27 @@ models_evalues$`ARIMA(0,0,1)`
 models_evalues$`ARIMA(1,0,1)`
 # R�sidus non corr�l�s globalement mais coefficient ma1 pas significatif
 
-models_evalues$`ARIMA(2,0,1)`
-# R�sidus non corr�l�s globalement et coefficients significatifs � 5% 
-
-models_evalues$`ARIMA(0,0,2)`
-# R�sidus corr�l�s mais coefficients significatifs
-
-models_evalues$`ARIMA(1,0,2)`
-# R�sidus non corr�l�s globalement mais coefficients pas significatifs
-
-models_evalues$`ARIMA(2,0,2)`
-# Tr�s peu d'autocorr�lation mais coefficients ar1 et ma1 non significatifs
-
-models_evalues$`ARIMA(0,0,3)`
-# Tr�s peu d'autocorr�lation mais coefficient ma3 non significatif
-
-models_evalues$`ARIMA(1,0,3)`
-# Coefficients pas significatifs
-
-models_evalues$`ARIMA(2,0,3)`
-# Tr�s peu d'autocorr�lation mais coefficient ar1 non significatif
- 
-nom_modeles_retenus = c('ARIMA(1,0,0)', 'ARIMA(0,0,1)', 'ARIMA(2,0,1)')
+nom_modeles_retenus = c('ARIMA(1,0,0)', 'ARIMA(0,0,1)')
 
 modeles_retenus <- models_evalues[nom_modeles_retenus]
 
+# Selection par la qualité (AIC, BIC)
 qualite_modeles_retenus <- sapply(modeles_retenus, function(x) x$qualite)
-
 round(qualite_modeles_retenus,4)
-
 apply(qualite_modeles_retenus,1,function(x) colnames(qualite_modeles_retenus)[which.min(x)])
-# Sur les crit�re d'informations un AR(1) semble le meilleur choix : ce r�sultat 
-# correspond aussi � l'hypoth�se de d�part �tant l'ACF.
+# Sur les critère d'informations un AR(1) semble le meilleur choix : ce résultat 
+# correspond aussi à l'hypothèse de départ avec l'ACF.
 
 # Question 5
 
 # Finalement on opte pour un ARIMA(1,1,0)
-model <- Arima(ts_data, order = c(1,1,0))
+model <- Arima(diff_ts, order = c(1,1,0))
 model
-checkresiduals(model)
 
-# Partie 3 : Pr�diction####
+checkresiduals(model)
+# Les résidues sont bien gaussiens
+
+# Partie 3 : Prédiction####
 
 # Question 6 :..
 lapply(modeles_retenus,function(x) forecast(x$model, h = 4))
